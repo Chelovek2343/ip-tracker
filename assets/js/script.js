b825be6782c4ec4aa74151b663179bb8bb278119
Ulugbek <79435067+Chelovek2343@users.noreply.github.com>
2023-01-19 12:34:13 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,16 @@ const local = document.getElementById('local');
 const timezone = document.getElementById('timezone');
 const isp = document.getElementById('isp');
 
-getIpGeo();
+const form = document.querySelector('form');
+const input = document.getElementById('inputInfo');
 
-async function getIpGeo(){
-    const response = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_m3mLJ13Bwn59hEj2BGoGtZ301U7qU');
+getIpGeo();
+getSearch();
+
+async function getIpGeo() {
+    const response = await fetch(
+        'https://geo.ipify.org/api/v2/country,city?apiKey=at_m3mLJ13Bwn59hEj2BGoGtZ301U7qU'
+    );
     const data = await response.json();
 
     ipAddress.innerHTML = data.ip;
@@ -15,11 +21,37 @@ async function getIpGeo(){
     isp.innerHTML = data.isp;
 }
 
+function getSearch() {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        getRequest();
+
+        async function getRequest() {
+            const resp = await fetch(
+                `https://api.ipbase.com/v2/info?ip=${input.value}&apikey=cv0RpW6T20kzcXXNeFMGoeI33xLCS1vBmkO7ZKSJ`
+            );
+
+            const respData = await resp.json();
+
+            const utcZone = respData.data.timezone.current_time.slice(19, 25);
+
+            ipAddress.innerHTML = respData.data.ip;
+            local.innerHTML = `${respData.data.timezone.id}`;
+            timezone.innerHTML = `UTC ${utcZone}`;
+            isp.innerHTML = respData.data.connection.isp;
+
+            console.log(respData);
+        }
+    });
+}
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
