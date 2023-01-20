3656d1a9e437f83743925bc2b9c01dcc80bcf129
Ulugbek <79435067+Chelovek2343@users.noreply.github.com>
2023-01-20 19:39:45 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,10 @@ const ipAddress = document.getElementById('ipAddress');
 const local = document.getElementById('local');
 const timezone = document.getElementById('timezone');
 const isp = document.getElementById('isp');
-
 const form = document.querySelector('form');
 const input = document.getElementById('inputInfo');
 
 getIpGeo();
-getSearch();
 
 async function getIpGeo() {
     const response = await fetch(
@@ -19,39 +17,59 @@ async function getIpGeo() {
     local.innerHTML = `${data.location.country}, ${data.location.region}`;
     timezone.innerHTML = `UTC ${data.location.timezone}`;
     isp.innerHTML = data.isp;
+
+    const map = L.map('map').setView(
+        [data.location.lat, data.location.lng],
+        13
+    );
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    const marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
 }
 
-function getSearch() {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-        getRequest();
+    getRequest();
 
-        async function getRequest() {
-            const resp = await fetch(
-                `https://api.ipbase.com/v2/info?ip=${input.value}&apikey=cv0RpW6T20kzcXXNeFMGoeI33xLCS1vBmkO7ZKSJ`
-            );
+    async function getRequest() {
+        const resp = await fetch(
+            `https://api.ipbase.com/v2/info?ip=${input.value}&apikey=cv0RpW6T20kzcXXNeFMGoeI33xLCS1vBmkO7ZKSJ`
+        );
 
-            const respData = await resp.json();
+        const respData = await resp.json();
 
-            const utcZone = respData.data.timezone.current_time.slice(19, 25);
+        const utcZone = respData.data.timezone.current_time.slice(19, 25);
 
-            ipAddress.innerHTML = respData.data.ip;
-            local.innerHTML = `${respData.data.timezone.id}`;
-            timezone.innerHTML = `UTC ${utcZone}`;
-            isp.innerHTML = respData.data.connection.isp;
+        ipAddress.innerHTML = respData.data.ip;
+        local.innerHTML = `${respData.data.timezone.id}`;
+        timezone.innerHTML = `UTC ${utcZone}`;
+        isp.innerHTML = respData.data.connection.isp;
 
-            console.log(respData);
-        }
-    });
+        const requestMap = L.map('map').setView(
+            [respData.data.location.latitude, respData.data.location.longitude],
+            13
+        );
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution:
+                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(requestMap);
+
+        const marker = L.marker([
+            respData.data.location.latitude,
+            respData.data.location.longitude,
+        ]).addTo(requestMap);
+    }
+});
+
+const container = L.DomUtil.get('map');
+if (container != null) {
+    container._leaflet_id = null;
 }
-
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
